af3a69a78fd8828d8b4043e7fbc7e9f912ae225b
Nick <nicholasmorris2599@gmail.com>
2022-11-04 17:43:32 -0700

    Created random value generators for all values going to be used.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,26 @@
-// Assignment code here
+// Random value generators
 
+var randomLowerValue = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26 + 97));   
+}
+
+var randomUpperValue = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+}
+
+var randomNumValue = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+}
+
+var randomSymbolValue = function() {
+  var symbolSet = '!@#$%^&*?<>[]{}=,.';
+  return symbolSet[Math.floor(Math.random() * symbolSet.length)];
+}
+
+
+var generatePassword = function() {
+  var userInputLength = window.prompt("Length of password?")
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,8 +31,9 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
